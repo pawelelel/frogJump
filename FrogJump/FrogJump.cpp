584d0aba3f5f2b077c77a01a5c18a98ae094cd9d
PAWEL\pawci <pawel.leczkowski2005@gmail.com>
2024-11-10 17:53:23 +0100

    basic game template

--- FrogJump/FrogJump.cpp
+++ FrogJump/FrogJump.cpp
@@ -1,6 +1,45 @@
+#include <cmath>
 #include <stdio.h>
+#include <conio.h>
+
+struct GameVariables
+{
+	int width, height;
+	char** screen;
+	bool run;
+};
+
+void PrintGameFrame(GameVariables game)
+{
+	printf("%d\n", game.height);
+}
+
+void GameFrame(GameVariables game)
+{
+	PrintGameFrame(game);
+}
+
+bool Init(GameVariables &game)
+{
+	game.width = 10;
+	game.height = 10;
+	game.run = true;
+	return true;
+}
 
 int main()
 {
-    
+	GameVariables game = {};
+	
+	if (!Init(game))
+	{
+		return -1;
+	}
+
+	while (game.run)
+	{
+		GameFrame(game);
+	}
+
+	return 0;
 }
